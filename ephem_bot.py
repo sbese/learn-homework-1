6118a80bf9db4921200ecbeb9acee49fbbd4e1e2
sbese <sergbuks2@gmail.com>
2020-02-20 18:59:52 +0300

    выполнено задание ephem_bot.py

--- ephem_bot.py
+++ ephem_bot.py
@@ -13,29 +13,51 @@
 
 """
 import logging
+import ephem
+
+from datetime import datetime
+# import time
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    filename='bot.log'
-                    )
+logging.basicConfig(
+    format="%(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    filename="bot.log",
+)
 
 
-PROXY = {'proxy_url': 'socks5://t3.learn.python.ru:1080',
-         'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+PROXY = {
+    "proxy_url": "socks5://t1.learn.python.ru:1080",
+    "urllib3_proxy_kwargs": {"username": "learn", "password": "python"},
+    "read_timeout": 6,
+    "connect_timeout": 7,
+}
 
 
 def greet_user(bot, update):
-    text = 'Вызван /start'
+    text = "Вызван /start"
     print(text)
     update.message.reply_text(text)
 
 
 def planet(bot, update):
     user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(user_text)
+    print("planet", user_text)
+    print(user_text.split(" "))
+    if user_text. len(user_text.split(" ")) != 2:
+        update.message.reply_text("enter valid planet name")
+        return
+
+    planet_name = user_text.split(" ")[1].capitalize()
+    try:
+        planet_obj = getattr(ephem, planet_name)(datetime.now().strftime("%d/%m/%Y"))
+    except AttributeError:
+        update.message.reply_text("enter valid planet name")
+        return
+    planet_obj.compute()
+    planet_constellation = ephem.constellation(planet_obj)
+    update.message.reply_text(planet_constellation[1])
 
 
 def talk_to_me(bot, update):
@@ -46,13 +68,12 @@ def talk_to_me(bot, update):
 
 def main():
     mybot = Updater(
-        "377500143:AAEaq5uyS4o8v7c2Q-EyLfJQ6Nyi1GK8jf4", request_kwargs=PROXY)
-
-    # dp = mybot.dispatcher
-    # dp.add_handler(CommandHandler("start", greet_user))
-    # dp.add_handler(CommandHandler("start", planet))
-    # dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-
+        "1081465226:AAHeq3mmCCTIHXT2V-3heAHULBjgAhgYg7Y", request_kwargs=PROXY
+    )
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     mybot.start_polling()
     mybot.idle()
 
